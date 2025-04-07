3b6f783e2aad37308656ca6a9565f5b332408a83
Nicolas B <97320296+NickBzy@users.noreply.github.com>
2025-04-07 17:27:38 -0400

    Add HTML skeleton and grid sections

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/scripts/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
@@ -30,6 +30,10 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
             },
         ],
